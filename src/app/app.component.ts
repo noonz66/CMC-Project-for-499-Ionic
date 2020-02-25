9e70b073224c3eabec09cb58418a13fe0679c2cc
NooraSajjad <noora.sajjad@gmail.com>
2020-02-25 03:18:28 +0300

    fixed android splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+//import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+//import { Plugins } from '@capacitor/core';
+//const { SplashScreen } = Plugins;
 
 @Component({
 	selector: "app-root",
@@ -53,7 +56,6 @@ export class AppComponent {
 
 	constructor(
 		private platform: Platform,
-		private splashScreen: SplashScreen,
 		private statusBar: StatusBar
 	) {
 		this.initializeApp();
@@ -62,8 +64,7 @@ export class AppComponent {
 	initializeApp() {
 		this.platform.ready().then(() => {
 			this.statusBar.styleDefault();
-			this.splashScreen.hide();
-
+			//SplashScreen.hide();
 			
 		});
 	}
