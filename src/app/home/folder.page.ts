c5d9a1122dd229819a069dfc954a564ac839a37b
NooraSajjad <noora.sajjad@gmail.com>
2020-04-24 05:41:58 +0300

    fixed default font, recreated app icon, did in app browser routing with options done

--- src/app/home/folder.page.ts
+++ src/app/home/folder.page.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
+
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+	selector: "app-folder",
+	templateUrl: "./folder.page.html",
+	styleUrls: ["./folder.page.scss"],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+	public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+	constructor(
+		private activatedRoute: ActivatedRoute,
+		private iab: InAppBrowser
+	) {}
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+	openTawasul(){
+    this.iab.create(
+			"https://services.bahrain.bh/wps/portal/tawasul/",
+			"_self",
+			"toolbarposition=top,presentationstyle=fullscreen,toolbar=yes,toolbarcolor=#AC3236,navigationbuttoncolor=#ffffff,enableViewportScale=yes,hideurlbar=yes,closebuttoncaption=إرجاع,closebuttoncolor=#ffffff,footer=yes,footercolor=#AC3236,fullscreen=yes,location=yes,keyboardDisplayRequiresUserAction=yes,mediaPlaybackRequiresUserAction=yes,lefttoright=yes,shouldPauseOnSuspend=yes"
+		);
   }
 
+	ngOnInit() {
+		this.folder = this.activatedRoute.snapshot.paramMap.get("id");
+	}
 }
